7f14b4848c818288d3b47ccab73c98375daca6be
tomasjac24 <tomas.jac@educhem.cz>
2023-03-15 12:57:04 +0100

    1.0.1

--- Teplota/Program.cs
+++ Teplota/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Spectre.Console;
 //Výběr měsíce
+Zpět:
 var mesic = AnsiConsole.Prompt(
     new SelectionPrompt<string>()
         .Title("Vyber měsíc do kterého chceš zadat teplotu.")
@@ -9,137 +10,42 @@ var mesic = AnsiConsole.Prompt(
             "Leden", "Únor","Březen","Duben","Květen", "Červen", "Červenec", "Srpen", "Září", "Říjen", "Listopad", "Prosinec"
         }));
 //Dny v měsíci
-string[] dny28 = {"1.den","2.den","3.den","4.den","5.den","6.den","7.den","8.den","9.den","10.den","11.den","12.den","13.den","14.den",
-            "15.den","16.den","17.den","18.den","19.den","20.den","21.den","22.den","23.den","24.den","25.den","26.den","27.den","28.den",};
-string[] dny30 = {"1.den","2.den","3.den","4.den","5.den","6.den","7.den","8.den","9.den","10.den","11.den","12.den","13.den","14.den",
-            "15.den","16.den","17.den","18.den","19.den","20.den","21.den","22.den","23.den","24.den","25.den","26.den","27.den","28.den",
-            "29.den","30.den"};
-string[] dny31 = {"1.den","2.den","3.den","4.den","5.den","6.den","7.den","8.den","9.den","10.den","11.den","12.den","13.den","14.den",
-            "15.den","16.den","17.den","18.den","19.den","20.den","21.den","22.den","23.den","24.den","25.den","26.den","27.den","28.den",
-            "29.den","30.den","31.den"};
+string[] dny28 = {"Únor"};
+string[] dny30 = {"Duben", "Červen", "Září", "Listopad" };
+string[] dny31 = {"Leden", "Březen", "Květen", "Říjen", "Prosinec"};
 
-switch (mesic)
+int dnyVMesici = 0;
+if (dny28.Contains(mesic)) dnyVMesici = 28;
+if (dny30.Contains(mesic)) dnyVMesici = 30;
+if (dny31.Contains(mesic)) dnyVMesici = 31;
+
+var dny = new List<string>();
+var stupne = new List<float>();
+//Výběr dne
+dny.Add("Zpět");
+for (int i = 1; i <= dnyVMesici; i++) dny.Add($"{i}.den");
+dny.Add("Konec");
+bool opakovani = true;
+while (opakovani)
 {
-    case "Leden":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny31
-        );
-
+    Console.Clear();
+    var den = AnsiConsole.Prompt(
+        new SelectionPrompt<string>()
+            .Title("Vyber den do kterého chceš zadat teplotu.")
+            .PageSize(10)
+            .MoreChoicesText("[grey](Vyber den šipkami)[/]")
+            .AddChoices(dny));
+    if(den=="Zpět")
+    {
+        goto Zpět;
+    }
+    if (den == "Konec")
+    {
+        opakovani = false;
         break;
-
-    case "Únor":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny28
-        );
-        break;
-    
-    case "Březen":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny31
-        );
-        break;
-
-    case "Duben":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny30
-        );
-        break;
-
-    case "Květen":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny31
-        );
-        break;
-
-    case "Červen":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny30
-        );
-        break;
-
-    case "Červenec":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny31
-        );
-        break;
-
-    case "Srpen":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny31
-        );
-        break;
-
-    case "Září":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny30
-        );
-        break;
-
-    case "Říjen":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny31
-        );
-        break;
-
-    case "Listopad":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny30
-        );
-        break;
-
-    case "Prosinec":
-    new SelectionPrompt<string>()
-        .Title("Vyber den do kterého chceš zadat teplotu.")
-        .PageSize(10)
-        .MoreChoicesText("[grey](Vyber den šipkami)[/]")
-        .AddChoices(
-            dny31
-        );
-        break;
-
-
+    }
+    float stupen = AnsiConsole.Ask<float>("Vypiš teplotu dne:");
+    int pozice = dny.IndexOf(den);
+    stupne.Add(stupen);
+    dny[pozice] = $"{pozice + 1}.den = {stupen}°C";
 }
